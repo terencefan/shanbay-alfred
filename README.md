c0dcaf95958f75ca251be4a5dd09f296520d148a
Fan <fantengyuan@baixing.com>
2016-06-14 15:11:18 +0800

    add word

--- README.md
+++ README.md
@@ -11,3 +11,13 @@
 代码库里有个叫做**Shanbay.alfredworkflow**的文件，下载下来打开就行了
 
 但首先你要有个alfred
+
+## 如何使用添加至词库和查例句功能
+
+> ccopen
+
+根据提示进行，当你看到Shanbay OAuth2时，copy浏览器url中的access\_token
+
+> ccauth {your\_access\_token}
+
+成功后，会弹出授权成功的提示
--- shanbay.py
+++ shanbay.py
@@ -6,21 +6,25 @@ __author__ = "stdrickforce"  # Tengyuan Fan
 
 import argparse
 import json
+import os
 import sys
-import urllib
-import urllib2
+import time
 
-from workflow import (
-    Workflow as WorkflowBase,
-)
+from workflow import Workflow as WorkflowBase
+from workflow import web
+from workflow.notify import notify
+
+TOKEN_FILE = os.path.abspath('access_token')
 
 
 class Workflow(WorkflowBase):
 
-    def push(self, title, subtitle):
+    def push(self, title, subtitle, query=''):
         self.add_item(
             title=title,
             subtitle=subtitle,
+            arg=query,
+            valid=True,
             icon='shanbay_favicon.png',
         )
 
@@ -32,46 +36,93 @@ class Shanbay(object):
     def __init__(self, wf):
         self.wf = wf
 
+    @property
+    def access_token(self):
+        if not os.path.isfile(TOKEN_FILE):
+            return ''
+        with open(TOKEN_FILE) as f:
+            data = json.loads(f.read().strip())
+        if time.time() < data['expire_at']:
+            return data['access_token']
+        return ''
+
     def get(self, action, params):
-        url = self.HOST + action + '/?' + urllib.urlencode(params)
-        content = urllib2.urlopen(url).read()
-        return json.loads(content)
+        url = self.HOST + action + '/'
+        return web.get(url, params=params).json()
+
+    def post(self, action, params={}, data={}):
+        url = self.HOST + action + '/'
+        return web.post(url, params=params, data=data).json()
 
     def query(self, word):
         try:
             res = self.get('search', {'word': word})
             wf.push(
-                title=res['data']['definition'],
-                subtitle="us: [%s], uk: [%s]" % (
+                res['data']['definition'],
+                "us: [%s], uk: [%s]" % (
                     res['data']['pronunciations']['us'],
                     res['data']['pronunciations']['uk'],
                 ),
+                str(res['data']['id']),
             )
             return res['data']['id']
         except:
             wf.push(
-                title='Ops! The word not exists!',
-                subtitle='please check your spelling mistakes.',
+                'Ops! The word not exists!',
+                'please check your spelling mistakes.',
             )
+
+    def add(self, wid):
+        if not self.access_token:
+            notify(
+                u'您必须填写授权码才能添加单词进词库',
+                u'请访问github主页了解详情',
+            )
+            return
+        self.post('learning', {
+            'access_token': self.access_token,
+        }, {
+            'id': wid,
+        })
 
     def query_example(self, wid):
         try:
-            res = self.get('example', {'vocabulary_id': wid})
+            res = self.get('example', {
+                'vocabulary_id': wid,
+                'access_token': self.access_token,
+            })
             for example in res['data']:
-                wf.add_item(title=example['annotation'])
-        except Exception:
+                title = example['annotation']
+                title = title.replace('<vocab>', '[')
+                title = title.replace('</vocab>', ']')
+                wf.push(title, example['translation'])
+        except Exception as e:  # noqa
             pass
+
+    def auth(self, token):
+        with open(TOKEN_FILE, 'w') as f:
+            f.write(json.dumps({
+                'access_token': token,
+                'expire_at': int(time.time()) + 24 * 30 * 60 * 60,
+            }))
+        notify(u'授权成功', u'您已成功授权，有效期为30天')
 
 
 def main(wf):
 
     parser = argparse.ArgumentParser()
+    parser.add_argument('command')
     parser.add_argument('query')
     args = parser.parse_args(wf.args)
 
     c = Shanbay(wf)
-    wid = c.query(args.query)
-    c.query_example(wid)
+    if args.command == 'query':
+        wid = c.query(args.query)
+        c.query_example(wid)
+    elif args.command == 'add':
+        c.add(args.query)
+    elif args.command == 'auth':
+        c.auth(args.query)
 
     # Send the results to Alfred as XML
     wf.send_feedback()
